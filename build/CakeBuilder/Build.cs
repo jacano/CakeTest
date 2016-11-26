8fe873e14b496eb276bb717d460eaf460e2291a0
jacanosalado@gmail.com <jacanosalado@gmail.com>
2016-11-26 13:30:37 +0100

    plist and codecake move

--- build/CakeBuilder/Build.cs
+++ build/CakeBuilder/Build.cs
@@ -4,11 +4,13 @@ using Cake.Common.Tools.MSBuild;
 using Cake.Common.Tools.NuGet;
 using Cake.Common.Diagnostics;
 using Cake.Common.Net;
+using Cake.Common.Build.TeamCity;
 using Cake.Core;
 using Cake.Core.Diagnostics;
 using Cake.Core.IO;
 
 using CodeCake;
+using System.Linq;
 
 namespace CakeBuilder
 {
@@ -19,11 +21,13 @@ namespace CakeBuilder
 
         public Build()
         {
+            var buildNumber = Cake.EnvironmentVariable("BUILDNUMBER") ?? "1";
+
             var artifactDir = Cake.Directory("artifacts");
             var outputDir = Cake.Directory("output");
             var toolsDir = Cake.Directory("tools");
 
-            var configuration = Cake.Argument("Configuration", "Release");
+            var configuration = Cake.Argument("Configuration", "Debug");
 
             Task("Clean").Does(() =>
             {
@@ -60,7 +64,7 @@ namespace CakeBuilder
                 Cake.Information("Building all existing .csproj files with '{0}' configuration", configuration);
                 foreach (var csproj in Cake.GetFiles("src/**/*.csproj"))
                 {
-                    var outputPath = outputDir.Path.MakeAbsolute(Cake.Environment).FullPath + "/" + csproj.GetFilenameWithoutExtension().FullPath + "/";
+                    var outputPath = outputDir.Path.MakeAbsolute(Cake.Environment).FullPath + "/" + csproj.GetFilenameWithoutExtension().FullPath + "_" + buildNumber + "/";
 
                     Cake.MSBuild(csproj.FullPath,
                         new MSBuildSettings()
@@ -72,8 +76,27 @@ namespace CakeBuilder
                 }
             });
 
+            Task("GenerateArtifactsAndMove")
+                .IsDependentOn("Build")
+                .Does(() =>
+                {
+                    foreach (var output in Cake.GetDirectories(outputDir.Path.FullPath + "/*"))
+                    {
+                        var outputName = artifactDir.Path + "/" + output.Segments.Last() + ".zip";
+                        Cake.Zip(output, outputName);
+                    } 
+                });
+
+            Task("UploadTeamCity")
+                .IsDependentOn("GenerateArtifactsAndMove")
+                .Does(() =>
+                {
+                    var teamCityProvider = new TeamCityProvider(Cake.Environment, Cake.Log);
+                    teamCityProvider.PublishArtifacts(artifactDir.Path.FullPath);
+                });
+
             Task("Default")
-            .IsDependentOn("Build");
+            .IsDependentOn("UploadTeamCity");
         }
     }
 }
